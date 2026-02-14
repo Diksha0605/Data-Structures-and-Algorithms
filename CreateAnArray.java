4a763cb4050efaa96ce4fd24c5406ea32504b56b
Diksha0605 <dikshasahu0605@gmail.com>
2026-02-15 00:58:12 +0530

    Update CreateAnArray.java

--- CreateAnArray.java
+++ CreateAnArray.java
@@ -1,1 +1,20 @@
+public class CreateAnArray {
+    public static void main(String[] args) {
+
+        // Creating an array
+        int[] arr = new int[5];
+
+        // Assigning values
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        // Printing array elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
 
